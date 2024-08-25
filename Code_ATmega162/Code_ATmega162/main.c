c19ae8236bc831b9416c3d3b3ea65a00628e5d2a
PizzaAllTheWay <karate.martynas1@gmail.com>
2024-08-26 00:58:30 +0200

    Start of the project :)

---
+++ Code_ATmega162/Code_ATmega162/main.c
@@ -0,0 +1,18 @@
+/*
+ * Code_ATmega162.c
+ *
+ * Created: 26/08/2024 00:52:18
+ * Author : Martynas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Ping Pong Project
+A School project for subject TTK4155. The goal of the project is to teach students how to develop and design Embedded Systems
+
+Lord Have mercy on us
+Let the grind BEGIN!

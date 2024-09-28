fa6035fb4a693f5c534f5c79b94887c393590c9a
Benjamin-A <benjamin.roald.andersen@gmail.com>
2024-09-28 13:02:11 +0200

    state machine for menu

--- Code_ATmega162/Code_ATmega162/Drivers/Menu/menu.c
+++ Code_ATmega162/Code_ATmega162/Drivers/Menu/menu.c
@@ -4,3 +4,32 @@
  * Created: 23/09/2024 18:23:01
  *  Author: Martynas
  */ 
+#include "menu.h"
+
+int menu_loop (){
+    // Infinite loop
+    while (1)
+    {
+        switch (menu_state)
+        {
+           case PING_PONG:
+               break;
+           case BONGO_CAT:
+               break;
+           case NYAN_CAT:
+               break;
+           case GHOST:
+               break;
+           default:
+               break;
+        
+    }
+    
+
+
+
+
+
+
+    return 0;
+}
--- Code_ATmega162/Code_ATmega162/Drivers/Menu/menu.h
+++ Code_ATmega162/Code_ATmega162/Drivers/Menu/menu.h
@@ -9,8 +9,19 @@
 #ifndef MENU_H_
 #define MENU_H_
 
+#include "Drivers/Menu/sprite_ghost.h"
+#include "Drivers/Menu/sprite_nyan_cat.h"
+#include "Drivers/Menu/sprite_bongo_cat.h"
 
 
+int menu_loop ();
 
+enum menu_state {
+    PING_PONG, 
+    BONGO_CAT, 
+    NYAN_CAT, 
+    GHOST,
+    MAX_MENU_STATE = GHOST // Ensure this is always the last value
+};
 
 #endif /* MENU_H_ */

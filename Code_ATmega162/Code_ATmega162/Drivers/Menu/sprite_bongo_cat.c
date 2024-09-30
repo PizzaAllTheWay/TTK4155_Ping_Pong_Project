38d718a01d15e47e4128256db928d9e10dd17a95
Benjamin-A <benjamin.roald.andersen@gmail.com>
2024-09-30 15:29:30 +0200

    moved bongo cat to allow reading of menu label

--- Code_ATmega162/Code_ATmega162/Drivers/Menu/sprite_bongo_cat.c
+++ Code_ATmega162/Code_ATmega162/Drivers/Menu/sprite_bongo_cat.c
@@ -9,8 +9,8 @@
 
 void sprite_bongo_cat(){
 	
-	oled_set_sprite(sprite_bongo_cat_0, 8, 8, 32, 0); // 8x8 nibbles, 1 nibble = 8 pixels
-	oled_set_sprite(sprite_bongo_cat_1, 8, 8, 32, 0); // 8x8 nibbles, 1 nibble = 8 pixels
+	oled_set_sprite(sprite_bongo_cat_0, 8, 8, 63, 0); // 8x8 nibbles, 1 nibble = 8 pixels
+	oled_set_sprite(sprite_bongo_cat_1, 8, 8, 63, 0); // 8x8 nibbles, 1 nibble = 8 pixels
 	//oled_set_sprite(sprite_bongo_cat_inverted_0, 8, 8, 32, 0); // 8x8 nibbles, 1 nibble = 8 pixels
 	//oled_set_sprite(sprite_bongo_cat_inverted_1, 8, 8, 32, 0); // 8x8 nibbles, 1 nibble = 8
 	//oled_clear_screen();

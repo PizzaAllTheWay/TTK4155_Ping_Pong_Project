8b5df8169d4e21b4c2a849d30b447f4a7d3bcc49
PizzaAllTheWay <karate.martynas1@gmail.com>
2024-10-07 14:20:51 +0200

    Building blocks built

--- Code_ATmega162/Code_ATmega162/Drivers/SPI/spi_driver.c
+++ Code_ATmega162/Code_ATmega162/Drivers/SPI/spi_driver.c
@@ -7,25 +7,84 @@
 
 #include "spi_driver.h"
 
-//init <3
+	//SS pb4 - device
+	//MOSI pb5
+	//MISO pb6
+	//SCK Pb7
+	//SPCR is the SPI control register
+	//SPSR is the SPI status register
+	//SPDR is the SPI data register
+	//SPIF SPI interrupt flag
+	//CPOL clock polarity
+	//CPHA clock phase
+	//PRR1/0 and SPI2X are used to specify the clock rate
+
+
 void spi_init(){
 	//set MOSI and SCK as output in DDR
-	
+	DDR_SPI |= (1<<DD_MOSI)|(1<<DD_SCK);
+	DDRB |= (1<<SLAVE_PIN);
 	//enable SPI, set as master
+	//Prescaler: Fosc/16, Enable interrupts
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 }
 
-//config functions
-void spi_set_mode(uint8_t mode);
-void spi_set_clock_divider(uint8_t divider);
+void spi_set_mode(uint8_t mode){
+	//we want mode 0 0
+	//sample rising edge
+	//setup falling edge
+	SPCR = (SPCR & ~((1<<CPOL)|(1<<CPHA))) | (mode & 0x03);
+}
 
-//data transfer functions
-void spi_transfer(uint8_t data);
-void spi_transfer_buffer(uint8_t *data_out, uint8_t *data_in, size_t length);
+void spi_set_clock_divider(uint8_t divider){
+	SPCR = (SPCR & ~((1<<SPR1)|(1<<SPR0))) | (divider & 0x03);
+	SPSR = (SPSR & ~(1<<SPI2X)) | ((divider & 0x04) ? (1<<SPI2X) : 0);
+}
 
-//chip select functions
-void spi_select_device(uint8_t device);
-void spi_deselect_device(uint8_t device);
+
+// transmit a single byte
+void spi_master_transmit(uint8_t data){
+	// Start transmission 
+	SPDR = data;
+	// Wait for transmission complete 
+	while(!(SPSR & (1<<SPIF)));
+}
+
+uint8_t spi_master_recieve(){
+	spi_master_transmit(0x0);
+	return(SPDR);
+}
+
+// transfer a single byte
+uint8_t spi_transfer(uint8_t data){
+	// Start transmission
+	SPDR = data;
+	//Wait for transmission complete
+	while(!(SPSR & (1<<SPIF)));
+
+	//return recieved data
+	return(SPDR);
+}
+
+// transfer a buffer of data
+void spi_transfer_buffer(uint8_t *data_out, uint8_t *data_in, size_t length){
+	for(size_t i=0; i < length; i++){
+		data_in[i] = spi_transfer(data_out[i]);
+	}
+}
+
+// Select SPI device
+void spi_select_device(){
+	PORTB &= ~(1<<SLAVE_PIN);
+}
+
+// Deselect SPI device
+void spi_deselect_device(){
+	PORTB |= ~(1<<SLAVE_PIN);
+}
 
 
 //error handling function
-void spi_get_status();
+uint8_t spi_get_status(){
+	return SPSR;
+}
--- Code_ATmega162/Code_ATmega162/Drivers/SPI/spi_driver.h
+++ Code_ATmega162/Code_ATmega162/Drivers/SPI/spi_driver.h
@@ -13,6 +13,8 @@
 #include <stdio.h>
 #include <util/delay.h>
 
+#define SLAVE_PIN PB4  
+
 //init <3
 void spi_init();
 
@@ -21,16 +23,17 @@ void spi_set_mode(uint8_t mode);
 void spi_set_clock_divider(uint8_t divider);
 
 //data transfer functions
-void spi_transfer(uint8_t data);
-void spi_transfer_buffer(uint8_t *data_out, uint8_t *data_in, size_t length);
+void spi_master_transfer(uint8_t data);
+uint8_t spi_master_recieve();
+uint8_t spi_transfer_buffer(uint8_t *data_out, uint8_t *data_in, size_t length);
 
 //chip select functions
-void spi_select_device(uint8_t device);
-void spi_deselect_device(uint8_t device);
+void spi_select_device();
+void spi_deselect_device();
 
 
 //error handling function
-void spi_get_status();
+uint8_t spi_get_status();
 
 
 #endif
